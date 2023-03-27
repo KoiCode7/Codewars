2de9209073fee59880d220855663609c7ef6d1be
github <github>
2023-03-27 13:51:29 -0400

    ♻️ Refactor code

--- Disemvowel Trolls.js
+++ Disemvowel Trolls.js
@@ -1,13 +1,4 @@
 function disemvowel(str) {
-  let newStr = str.replaceAll('a', '');
-  newStr = newStr.replaceAll('i', '');
-  newStr = newStr.replaceAll('u', '');
-  newStr = newStr.replaceAll('e', '');
-  newStr = newStr.replaceAll('o', '');
-    newStr = newStr.replaceAll('A', '');
-    newStr = newStr.replaceAll('I', '');
-    newStr = newStr.replaceAll('U', '');
-    newStr = newStr.replaceAll('E', '');
-    newStr = newStr.replaceAll('O', '');
+  const newStr = str.replace(/[aiueo]/gi, '');
   return newStr;
 }
