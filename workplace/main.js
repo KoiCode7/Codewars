fce0dcba1bd47b99e4df9d8d3538d25a4188a76e
github <github>
2023-03-28 11:32:03 -0400

    practice

--- workplace/main.js
+++ workplace/main.js
@@ -1,15 +1,24 @@
+/**
+ * Given the number of consecutive integers and the total of the integers,
+ * return the consecutive integer at the requested position.
+ *
+ * @param {int} x number of consecutive integers
+ * @param {int} y sum of consecutive integers
+ * @param {int} n position of requested integer
+ * @return {int} consecutive integer at requested position
+ */
+function position(x, y, n) {
+  let num = 0;
+  for(i= 0; i < x; i++) {
+    num += i;
+  }
 
+  const a = (y - num)/x;
 
+  return a + n;
 
-function removeSmallest(numbers) {
-  const arrayCopiedNumbers = [...numbers];
-  const smallestVal = Math.min(...arrayCopiedNumbers);
-  const index = arrayCopiedNumbers.indexOf(smallestVal);
-  arrayCopiedNumbers.splice(index, 1);
-  return arrayCopiedNumbers;
+  // TODO: return consecutive integer at requested position
 }
 
 
-console.log(removeSmallest([2,2,1,2,1]));
-
-
+console.log(position(3, -9, 1));
