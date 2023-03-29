da49baf4a95bb60a0a933150c23ee3ad8dad1bf7
github <github>
2023-03-29 11:08:58 -0400

    practice

--- workplace/main.js
+++ workplace/main.js
@@ -1,24 +1,14 @@
-/**
- * Given the number of consecutive integers and the total of the integers,
- * return the consecutive integer at the requested position.
- *
- * @param {int} x number of consecutive integers
- * @param {int} y sum of consecutive integers
- * @param {int} n position of requested integer
- * @return {int} consecutive integer at requested position
- */
-function position(x, y, n) {
-  let num = 0;
-  for(i= 0; i < x; i++) {
-    num += i;
-  }
 
-  const a = (y - num)/x;
 
-  return a + n;
 
-  // TODO: return consecutive integer at requested position
+function isPangram(string){
+let sentence = string.toLowerCase();
+sentence = sentence.match(/([a-z])(?!.*\1)/g);
+if(sentence.length === 26) {
+  return true;
+} else {
+  return false;
+}
 }
 
-
-console.log(position(3, -9, 1));
+console.log(isPangram("The quck brown fox jumps over the lazy dog"));
