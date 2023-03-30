b6b5d2404202c7af8325c0c626b344a40a595225
github <github>
2023-03-30 10:53:25 -0400

    practice

--- workplace/main.js
+++ workplace/main.js
@@ -1,20 +1,8 @@
 // Answer
 function getCount(str) {
-  let count = 0;
-
-  for (i = 0; i < str.length; i++) {
-    if (
-      (str[i] === "a") |
-      (str[i] === "i") |
-      (str[i] === "u") |
-      (str[i] === "e") |
-      (str[i] === "o")
-    ) {
-      count += 1;
-    }
-  }
-
-  return count;
+const count = str.match(/[aiueo]/gi);
+console.log(count);
+return count? count.length: 0;
 }
 
-getCount("abiuracadeoabra");
+getCount("bbb");
