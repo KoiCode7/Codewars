cc0d8f4614747079ae3fe509a38d4c2528d1498c
github <github>
2023-03-30 10:53:41 -0400

    Refactored a code

--- Vowel Count.js
+++ Vowel Count.js
@@ -10,19 +10,6 @@
 
 // Answer
 function getCount(str) {
-  let count = 0;
-
-  for (i = 0; i < str.length; i++) {
-    if (
-      (str[i] === "a") |
-      (str[i] === "i") |
-      (str[i] === "u") |
-      (str[i] === "e") |
-      (str[i] === "o")
-    ) {
-      count += 1;
-    }
+  const count = str.match(/[aiueo]/gi);
+  return count ? count.length : 0;
   }
-
-  return count;
-}
