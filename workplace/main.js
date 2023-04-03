e36970379af9a68067bef083e30e99f6b33225c0
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-03 11:10:31 -0400

    Test a code

--- workplace/main.js
+++ workplace/main.js
@@ -1,15 +1,6 @@
-// function largestPairSum(numbers) {
-//   const sortedArray = numbers.sort((a, b) => b - a);
-//   return sortedArray[0] + sortedArray[1];
-// }
-
-console.log(largestPairSum([10, 14, 2, 23, 19]));
-
-
-function largestPairSum(numbers) {
-  const [a, b, c] = numbers.sort((a, b) => b - a)
-  console.log(a);
-  console.log(b);
-  console.log(c);
-  return a + b
+function sumEvenNumbers(input) {
+  const evenNum = input.filter((el) => el % 2 === 0);
+  return Math.floor(evenNum.reduce((acc, c) => acc + c, 0));
 }
+
+console.log(sumEvenNumbers([4, 3, 1, 2, 5, 10, 6, 7, 9, 8]));
