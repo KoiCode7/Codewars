ef1a1f55f7679a5057230ec7e43fc927c1a28da3
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-04 11:16:02 -0400

    Test a code

--- workplace/main.js
+++ workplace/main.js
@@ -1,11 +1,5 @@
 function squareDigits(num) {
-  const str = num.toString();
-  let result = "";
-
-  for (let i = 0; i < str.length; i++) {
-    result += str[i] ** 2;
-  }
-  return +result;
+return +num.toString().split('').map(el => el ** 2).join('');
 }
 
 console.log(squareDigits(765));
