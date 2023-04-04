6c30f0ab9de9b7026cfedb61de3ac59cba1f1628
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-04 10:26:42 -0400

    Valid Parentheses

---
+++ Valid Parentheses.js
@@ -0,0 +1,41 @@
+// DESCRIPTION:
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+// Constraints
+// 0 <= length of input <= 100
+
+// All inputs will be strings, consisting only of characters ( and ).
+// Empty strings are considered balanced (and therefore valid), and will be tested.
+// For languages with mutable strings, the inputs should not be mutated.
+
+// Protip: If you are trying to figure out why a string of parentheses is invalid, paste the parentheses into the code editor, and let the code highlighting show you!
+
+
+// Answer
+function validParentheses(parenStr) {
+  let stack = [];
+  let pair = { "(": ")" };
+
+  for(let i = 0; i < parenStr.length; i++) {
+    if(parenStr[i] === '(') {
+      stack.push(parenStr[i])
+    } else {
+      const picked = stack.pop();
+
+      if(parenStr[i] !== pair[picked]) {
+        return false;
+      }
+    }
+  }
+
+  if(stack.length !== 0) {
+    return false;
+  }
+
+  return true;
+}
--- workplace/main.js
+++ workplace/main.js
@@ -1,5 +1,22 @@
-function betweenExtremes(numbers) {
-return Math.max(...numbers) - Math.min(...numbers)
-}
+function validParentheses(parenStr) {
+  let stack = [];
+  let pair = { "(": ")" };
 
-console.log(betweenExtremes([1, 434, 555, 34, 112]));
+  for(let i = 0; i < parenStr.length; i++) {
+    if(parenStr[i] === '(') {
+      stack.push(parenStr[i])
+    } else {
+      const picked = stack.pop();
+
+      if(parenStr[i] !== pair[picked]) {
+        return false;
+      }
+    }
+  }
+
+  if(stack.length !== 0) {
+    return false;
+  }
+
+  return true;
+}
