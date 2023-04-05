c1461419777864a2b37cb2879b56cad1b58c4ded
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-05 12:40:51 -0400

    Test a code

--- workplace/main.js
+++ workplace/main.js
@@ -1,6 +1,16 @@
-function squareDigits(num) {
-return +num.toString().split('').map(el => el ** 2).join('');
+function solution(start, finish) {
+  let count = 0;
+  let shelfNum = start;
+  while (finish !== shelfNum) {
+    if (finish >= shelfNum + 3) {
+      count++;
+      shelfNum += 3;
+    } else {
+      count++;
+      shelfNum += 1;
+    }
+  }
+  return count;
 }
 
-console.log(squareDigits(765));
-
+console.log(solution(1, 5));
