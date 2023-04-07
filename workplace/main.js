91eb28e05312f972c04dcae056b1c1c20c9668b7
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-07 11:38:18 -0400

    Test a code

--- workplace/main.js
+++ workplace/main.js
@@ -1,16 +1,9 @@
 function count(string) {
   const object = {};
-  for (let i = 0; i < string.length; i++) {
-    let char = string[i];
-
-    if (object[char]) {
-      object[char]++;
-    } else {
-      object[char] = 1;
-    }
-  }
+  string.split("").forEach((el) => {
+    object[el] ? object[el]++ : (object[el] = 1);
+  });
   return object;
 }
 
-
-console.log(count('aba'));
+console.log(count("aba"));
