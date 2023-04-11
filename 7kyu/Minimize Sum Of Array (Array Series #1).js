1b5e929635f9c1c93fbd91d75bb35eaf0b615923
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-11 12:40:37 -0400

    Refactored a code

--- 7kyu/Minimize Sum Of Array (Array Series #1).js
+++ 7kyu/Minimize Sum Of Array (Array Series #1).js
@@ -19,9 +19,9 @@
 // Answer 
 function minSum(arr) {
   let sum = 0;
-  const newArr = arr.sort((a, b) => a - b);
-  for (let i = 0; i < newArr.length / 2; i++) {
-    sum += newArr[i] * newArr[newArr.length - (1 + i)];
+  arr.sort((a, b) => a - b);
+  while (arr.length) {
+    sum += arr.shift() * arr.pop();
   }
   return sum;
 }
