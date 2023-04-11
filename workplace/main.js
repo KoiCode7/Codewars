192b3a00efaa2ab6f9d8925bec1e16495393b7ea
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-11 12:24:22 -0400

    Test a code

--- workplace/main.js
+++ workplace/main.js
@@ -1,63 +1,10 @@
-// function selReverse(array, length) {
-//   let reverseArray = [];
-//   const num = Math.ceil(array.length / length);
-//   for (let i = 0; i < num; i++) {
-//     reverseArray.push(...array.splice(0, length).reverse());
-//   }
-
-//   return reverseArray;
-// }
-
-function selReverse(array, length) {
-  if (length === 0) {
-    return;
+function minSum(arr) {
+  let sum = 0;
+  const newArr = arr.sort((a, b) => a - b);
+  for (let i = 0; i < newArr.length / 2; i++) {
+    sum += newArr[i] * newArr[newArr.length - (1 + i)];
   }
-  let reverseArray = [];
-  const num = Math.ceil(array.length / length);
-
-  for (let i = 0; i < num; i++) {
-    const start = i * length;
-    const end = start + length;
-    const subArray = array.slice(start, end).reverse();
-    reverseArray = reverseArray.concat(subArray);
-  }
-
-  return reverseArray;
-}
-console.log(selReverse([2, 4, 6, 8, 10, 12, 14, 16], 3));
-
-function selReverse(array, length) {
-  let reverseArray = [];
-  const num = Math.ceil(array.length / length);
-  for (let i = 0; i < num; i++) {
-    const start = i * length;
-    const end = start + length;
-    const subArray = array.slice(start, end).reverse();
-    reverseArray = reverseArray.concat(subArray);
-  }
-
-  return reverseArray;
+  return sum;
 }
 
-// console.log(selReverse([2,4,6,8,10,12,14,16], 3));
-
-// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
-
-// console.log(animals.slice(2));
-// // Expected output: Array ["camel", "duck", "elephant"]
-
-// console.log(animals);
-// // Expected output: Array ["camel", "duck"]
-
-function selReverse(array, length) {
-  if(length === 0) {
-    return array;
-  }
-  let reverseArray = [];
-  const num = Math.ceil(array.length / length);
-  for (let i = 0; i < num; i++) {
-    reverseArray = [...reverseArray, ...array.splice(0, length).reverse()]
-  }
-
-  return reverseArray;
-}
+console.log(minSum([12, 6, 10, 26, 3, 24]));
