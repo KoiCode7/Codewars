001d7d05518f35d023cc61db35dc30547114851a
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-11 10:41:41 -0400

    Test a code

--- workplace/main.js
+++ workplace/main.js
@@ -1,14 +1,63 @@
-function dotCalculator(equation) {
-  const [left, operator, right] = equation.split(" ");
+// function selReverse(array, length) {
+//   let reverseArray = [];
+//   const num = Math.ceil(array.length / length);
+//   for (let i = 0; i < num; i++) {
+//     reverseArray.push(...array.splice(0, length).reverse());
+//   }
 
-  const operators = {
-    "+": (a, b) => a + b,
-    "-": (a, b) => a - b,
-    "*": (a, b) => a * b,
-    "//": (a, b) => a / b,
-  };
+//   return reverseArray;
+// }
 
-  return ".".repeat(operators[operator](left.length, right.length));
+function selReverse(array, length) {
+  if (length === 0) {
+    return;
+  }
+  let reverseArray = [];
+  const num = Math.ceil(array.length / length);
+
+  for (let i = 0; i < num; i++) {
+    const start = i * length;
+    const end = start + length;
+    const subArray = array.slice(start, end).reverse();
+    reverseArray = reverseArray.concat(subArray);
+  }
+
+  return reverseArray;
+}
+console.log(selReverse([2, 4, 6, 8, 10, 12, 14, 16], 3));
+
+function selReverse(array, length) {
+  let reverseArray = [];
+  const num = Math.ceil(array.length / length);
+  for (let i = 0; i < num; i++) {
+    const start = i * length;
+    const end = start + length;
+    const subArray = array.slice(start, end).reverse();
+    reverseArray = reverseArray.concat(subArray);
+  }
+
+  return reverseArray;
 }
 
-console.log(dotCalculator(".. - .."));
+// console.log(selReverse([2,4,6,8,10,12,14,16], 3));
+
+// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+// console.log(animals.slice(2));
+// // Expected output: Array ["camel", "duck", "elephant"]
+
+// console.log(animals);
+// // Expected output: Array ["camel", "duck"]
+
+function selReverse(array, length) {
+  if(length === 0) {
+    return array;
+  }
+  let reverseArray = [];
+  const num = Math.ceil(array.length / length);
+  for (let i = 0; i < num; i++) {
+    reverseArray = [...reverseArray, ...array.splice(0, length).reverse()]
+  }
+
+  return reverseArray;
+}
