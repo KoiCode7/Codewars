08a70335291931a9bed6070535553772588f0692
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-12 11:18:53 -0400

    Test a code

--- workplace/main.js
+++ workplace/main.js
@@ -1,10 +1,8 @@
 function maxProduct(numbers, size) {
-  const sortedArr = numbers.sort((a, b) => b - a);
-  let result = 1;
-  for (let i = 0; i < size; i++) {
-    result *= sortedArr[i];
-  }
-  return result;
+  return numbers
+    .sort((a, b) => b - a)
+    .slice(0, size)
+    .reduce((acc, cu) => acc * cu, 1);
 }
 
 console.log(maxProduct([13, 12, -27, -302, 25, 37, 133, 155, -14], 5));
