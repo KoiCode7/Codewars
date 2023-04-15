71fc23b0295c08f6b690a55ded9c43c02f83e57e
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-15 09:17:48 -0400

    Test a code

--- workplace/main.js
+++ workplace/main.js
@@ -1,13 +1,4 @@
-function productArray(numbers) {
-  let result = [];
-
-  for (let i = 0; i < numbers.length; i++) {
-    const newArray = [...numbers];
-    newArray.splice(i, 1);
-    result = [...result, newArray.reduce((acc, c) => acc * c, 1)];
-  }
-
-  return result;
-}
+const productArray = (numbers) =>
+  numbers.map((el, idx, arr) => arr.reduce((acc, cu) => acc * cu, 1) / el);
 
 console.log(productArray([3, 27, 4, 2]));
