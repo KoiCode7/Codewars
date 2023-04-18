cd2b1293bb271967900e273344b4668f9f25be9c
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-18 11:31:08 -0400

    Test a code

--- workplace/main.js
+++ workplace/main.js
@@ -1,9 +1,7 @@
 function maxTriSum(numbers) {
-  return numbers
-    .sort((a, b) => b - a)
-    .filter((el, idx, arr) => el !== arr[idx - 1])
-    .filter((el, idx) => (idx < 3 ? el : null))
-    .reduce((acc, c) => acc + c, 0);
+  const arr = numbers.sort((a, b) => b - a).filter((el, idx, arr) => el !== arr[idx - 1]);
+
+  return arr[0] + arr[1] + arr[2];
 }
 
 console.log(maxTriSum([-7, 12, -7, 29, -5, 0, -7, 0, 0, 29]));
