d0ebb330eae908c560eb51c75eb12e3eb208b044
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-18 11:25:24 -0400

    Test a code

--- workplace/main.js
+++ workplace/main.js
@@ -1,12 +1,9 @@
-function minimumSteps(numbers, value) {
-  numbers.sort((a, b) => a - b);
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-    if (sum >= value) {
-      return i;
-    }
-  }
+function maxTriSum(numbers) {
+  return numbers
+    .sort((a, b) => b - a)
+    .filter((el, idx, arr) => el !== arr[idx - 1])
+    .filter((el, idx) => (idx < 3 ? el : null))
+    .reduce((acc, c) => acc + c, 0);
 }
 
-console.log(minimumSteps([19, 98, 69, 28, 75, 45, 17, 98, 67], 464));
+console.log(maxTriSum([-7, 12, -7, 29, -5, 0, -7, 0, 0, 29]));
