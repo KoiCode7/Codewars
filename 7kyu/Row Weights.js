6e84ba5f688bdc200ff0895bf0e3ca7cdf919d7a
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-19 14:50:15 -0400

    Refactored a code

--- 7kyu/Row Weights.js
+++ 7kyu/Row Weights.js
@@ -26,7 +26,6 @@
 
 // Answer 
 function rowWeights(array) {
-  console.log(array);
   const t1 = array.filter((el, idx) => idx % 2 === 0).reduce((acc, c) => acc + c, 0);
   const t2 = array.filter((el, idx) => idx % 2 !== 0).reduce((acc, c) => acc + c, 0);
   return [t1, t2];
--- workplace/main.js
+++ workplace/main.js
@@ -1,5 +1,4 @@
 function rowWeights(array) {
-  console.log(array);
   const t1 = array.filter((el, idx) => idx % 2 === 0).reduce((acc, c) => acc + c, 0);
   const t2 = array.filter((el, idx) => idx % 2 !== 0).reduce((acc, c) => acc + c, 0);
   return [t1, t2];
